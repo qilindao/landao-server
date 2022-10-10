acc49a3a7a20e5899b4a78b3fe894bf2f1119120
joycezhang <787027175@qq.com>
2022-10-10 17:56:32 +0800

    ✨ update

--- app/Services/Repositories/System/DictionaryRepo.php
+++ app/Services/Repositories/System/DictionaryRepo.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Services\Repositories\System;
 
+use App\Services\Enums\Common\GlobalCacheKeyEnum;
+use Illuminate\Support\Facades\Cache;
 use JoyceZ\LaravelLib\Repositories\BaseRepository;
 use App\Services\Repositories\System\Interfaces\IDictionary;
 use App\Services\Models\System\DictionaryModel;
@@ -24,5 +26,58 @@ class DictionaryRepo extends BaseRepository implements IDictionary
     public function model()
     {
         return DictionaryModel::class;
+    }
+
+    /**
+     * 根据字典类型进行数据分组
+     * @return array
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function getAllDictByGroup(): array
+    {
+        $dictionary = Cache::tags([GlobalCacheKeyEnum::ZODA_DICT_TAG])->get(GlobalCacheKeyEnum::ZODA_DICT_LIST_BY_GROUP);
+        if ($dictionary) {
+            return $dictionary;
+        }
+        $dictTypeRepo = $this->app->make(DictionaryTypeRepo::class);
+        $groupList = $dictTypeRepo->lists();
+        $group = array_column($groupList, 'type_key', 'dict_tid');
+        $dict = [];
+        $lists = $this->getList();
+        foreach ($lists as $item) {
+            $dict[$group[$item['type_id']]][] = $item;
+        }
+        if($dict){
+            Cache::tags([GlobalCacheKeyEnum::ZODA_DICT_TAG])->put(GlobalCacheKeyEnum::ZODA_DICT_LIST_BY_GROUP, $dict);
+        }
+        return $dict;
+    }
+
+    /**
+     * 获取所有字典数据
+     * @return array
+     */
+    public function getList()
+    {
+        $list = $this->all(['dict_id', 'type_id', 'label', 'is_enable', 'expand']);
+        $dict = [];
+        foreach ($list as $item) {
+            $dict[] = [
+                'label' => $item['label'],
+                'value' => $item['dict_id'],
+                'type_id' => $item['type_id'],
+                'is_enable' => $item['is_enable'],
+                'expand' => $item['expand'],
+            ];
+        }
+        return $dict;
+    }
+
+    /**
+     * 清除缓存
+     */
+    public function clearCache()
+    {
+        Cache::tags(GlobalCacheKeyEnum::ZODA_DICT_TAG)->flush();
     }
 }
--- app/Services/Repositories/System/RegionRepo.php
+++ app/Services/Repositories/System/RegionRepo.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App\Services\Repositories\System;
 
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\File;
 use JoyceZ\LaravelLib\Helpers\TreeHelper;
 use JoyceZ\LaravelLib\Repositories\BaseRepository;
@@ -29,17 +28,30 @@ class RegionRepo extends BaseRepository implements IRegion
         return RegionModel::class;
     }
 
+    /**
+     * 生成静态json数据
+     */
     public function buildLocal()
     {
         $region = $this->getRegionTree();
-        File::put('resources/region.json',json_encode($region));
+        File::put('resources/region.json', json_encode($region));
     }
 
     public function getRegionTree()
     {
-
         $district = $this->column('region_id as value,parent_id,fullname as label', [], 'region_id');
         return TreeHelper::listToTree($district, 0, 'value', 'parent_id');
+    }
+
+    /**
+     * 根据区域id数组，获取
+     * @param array $ids
+     * @return array
+     */
+    public function getRegionTreeByIds(array $ids): array
+    {
+        $lists = $this->findWhereIn('region_id', $ids, ['region_id as value', 'parent_id', 'fullname as label', 'depth']);
+        return TreeHelper::listToTree($lists->toArray(), 0, 'value', 'parent_id');
     }
 
 
